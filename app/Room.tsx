c95f7f45ab4c76a509b0bb56f8b1f0099f770883
Demilade01 <demiladeakinyemi2004@gmail.com>
2024-07-02 18:10:54 -0700

    implement Navbar icons && functionality

--- app/Room.tsx
+++ app/Room.tsx
@@ -6,12 +6,21 @@ import {
   RoomProvider,
   ClientSideSuspense,
 } from "@liveblocks/react/suspense";
+import { LiveMap } from "@liveblocks/client";
+import Loader from "@/components/Loader";
 
 export function Room({ children }: { children: ReactNode }) {
   return (
     <LiveblocksProvider publicApiKey={"pk_dev_9R99EH2vpmiCEiD4kMzHJhpvhbXoamcKrRXII5_UWL98eomdeV6FfjvD-U_0y3tL"}>
-      <RoomProvider id="my-room">
-        <ClientSideSuspense fallback={<div>Loading…</div>}>
+      <RoomProvider id="my-room"
+        initialPresence={{
+          cursor: null, cursorColor: null, editingText: null
+        }}
+        initialStorage={{
+          canvasObjects: new LiveMap()
+        }}
+      >
+        <ClientSideSuspense fallback={<Loader />}>
           {children}
         </ClientSideSuspense>
       </RoomProvider>
--- app/page.tsx
+++ app/page.tsx
@@ -5,8 +5,10 @@ import LeftSidebar from "@/components/LeftSidebar";
 import Live from "@/components/Live";
 import Navbar from "@/components/Navbar";
 import RightSidebar from "@/components/RightSidebar";
-import { useEffect, useRef } from "react";
-import { handleCanvasMouseDown, handleResize, initializeFabric } from '@/lib/canvas';
+import { useEffect, useRef, useState } from "react";
+import { handleCanvasMouseDown, handleCanvaseMouseMove, handleResize, initializeFabric } from '@/lib/canvas';
+import { ActiveElement } from '@/types/type';
+import { useMutation, useStorage } from '@/liveblocks.config';
 
 export default function Page() {
   const canvasRef = useRef<HTMLCanvasElement>(null);
@@ -14,6 +16,33 @@ export default function Page() {
   const isDrawing = useRef(false);
   const shapeRef = useRef<fabric.Object | null>(null);
   const selectedShapeRef = useRef<string | null>('rectangle');
+
+  const canvasObject = useStorage((root) => root.canvasObjects )
+
+  const syncShapeInStorage = useMutation(({ storage }, object) => {
+    if(!object) return
+
+    const { objectId } = object; 
+
+    const shapeData = object.toJSON();
+    shapeData.objectId = objectId;
+
+    const canvasObjects = storage.get('canvasObjects');
+
+    canvasObjects.set(objectId, shapeData);
+  }, [])
+
+  const [activeElement, setActiveElement] = useState<ActiveElement>({
+    name: '',
+    value: '',
+    icon:''
+  });
+
+  const handleActiveElement = (elem: ActiveElement) => {
+    setActiveElement(elem);
+
+    selectedShapeRef.current = elem?.value as string;
+  }
 
   useEffect(() => {
     const canvas = initializeFabric({ canvasRef, fabricRef })
@@ -25,7 +54,18 @@ export default function Page() {
       isDrawing,
       shapeRef,
       selectedShapeRef
+     })
     })
+
+    canvas.on("mouse:move", (options) => {
+     handleCanvaseMouseMove({
+      options,
+      canvas,
+      isDrawing,
+      shapeRef,
+      selectedShapeRef,
+      syncShapeInStorage
+     })
     })
 
     window.addEventListener("resize" , () => {
@@ -35,7 +75,10 @@ export default function Page() {
 
   return (
     <main className="h-screen overflow-hidden ">
-      <Navbar />
+      <Navbar 
+        activeElement={activeElement}
+        handleActiveElement={handleActiveElement}
+      />
 
       <section className="flex h-full flex-row">
         <LeftSidebar />
